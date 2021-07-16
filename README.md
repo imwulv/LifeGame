7a694343e1932658d7415748676897826892e983
imwulv <imwulv@gmail.com>
2021-07-16 14:49:17 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#生命游戏
+
+##简单的JAVA实现。
+Main函数直接启动就行
+
+相关参数在Constans中修改
+
+###二维数组长度宽
+WIDTH_SIZE
+HEIGHT_SIZE
+
+###控制绘制的细胞大小
+SQUARE_SIZE
+
+###随机生成存活细胞的个数
+LIFE_NUM
+
--- src/com/wulv/Constants.java
+++ src/com/wulv/Constants.java
@@ -2,11 +2,15 @@ package com.wulv;
 
 public class Constants {
 
+    //二维数组长度宽
     public static final int WIDTH_SIZE = 1024;
     public static final int HEIGHT_SIZE = 1024;
+    //控制绘制的细胞大小
     public static final int SQUARE_SIZE = 5;
+    //根据二维数组自动调整窗口大小
     public static final int WORLD_WIDTH = WIDTH_SIZE * SQUARE_SIZE;
     public static final int WORLD_HEIGHT = HEIGHT_SIZE * SQUARE_SIZE;
+    //随机生成存活细胞的个数
     public static final int LIFE_NUM = 1000000;
 
 }
